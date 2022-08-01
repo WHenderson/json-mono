635dce0424109d19b2f49ede19a881d7a85c041b
WHenderson <github@whenderson.maily.dev>
2022-08-01 15:30:56 +1000

    testing guards

---
+++ packages/json-pointer/test/guards.test.ts
@@ -0,0 +1,40 @@
+import {expect, it} from "vitest";
+import {is_absolute, is_pointer, is_relative, is_relative_iref, is_relative_pure} from "../src";
+import {inspect} from "node:util";
+
+it('should correctly identify example pointers', () => {
+    const guards = [
+        is_pointer,
+        is_absolute,
+        is_relative,
+        is_relative_iref,
+        is_relative_pure
+    ];
+
+    const values = [
+        { value: undefined, truthy: [] },
+        { value: 'a', truthy: [] },
+        { value: '0a', truthy: [] },
+        { value: '', truthy: [is_pointer, is_absolute] },
+        { value: '/', truthy: [is_pointer, is_absolute] },
+        { value: '/a/b/c', truthy: [is_pointer, is_absolute] },
+        { value: '0', truthy: [is_pointer, is_relative, is_relative_pure] },
+        { value: '0/', truthy: [is_pointer, is_relative, is_relative_pure] },
+        { value: '0/a/b/c', truthy: [is_pointer, is_relative, is_relative_pure] },
+        { value: '0#', truthy: [is_pointer, is_relative, is_relative_iref] },
+        { value: '00', truthy: [] },
+        { value: '01', truthy: [] },
+        { value: '123', truthy: [is_pointer, is_relative, is_relative_pure] },
+        { value: '123/', truthy: [is_pointer, is_relative, is_relative_pure] },
+        { value: '123/a/b/c', truthy: [is_pointer, is_relative, is_relative_pure] },
+        { value: '123#', truthy: [is_pointer, is_relative, is_relative_iref] },
+    ];
+
+    values.forEach(({ value, truthy }, index) => {
+        guards.forEach(guard => {
+            const is = guard(value);
+            const has = (<any[]>truthy).includes(guard);
+            expect(is, `${index}:${guard.name}(${inspect(value)})`).toBe(has);
+        });
+    });
+});
